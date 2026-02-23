f335df551b569656e4950f74e32cf0bbfee4770f
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-02-23 15:31:18 +0800

    first commit

---
+++ coba.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//variable global
+string nama;
+
+int main(){ //program utama
+    int age; //variable local
+    age = 19;
+
+    cout << "Halo World" << endl;
+    cout << 99 << endl;
+    cout << age;
+
+}
